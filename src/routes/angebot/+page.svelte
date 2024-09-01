850005ee588e0028aa48068f32b2bcf6272eccb0
menolina9 <menolina@proton.me>
2024-09-01 14:22:00 +0200

    feat: anpassungen Rechtschreibung % Sätze

--- src/routes/angebot/+page.svelte
+++ src/routes/angebot/+page.svelte
@@ -6,7 +6,7 @@
 <svelte:head>
 	<title>Angebot | Nora Hunziker</title>
 	<meta name="description"
-	content="Ich biete Einzelberatung an für Menschen, die immer wieder bei ähnlichen Schwierigkeiten anstehen, in einer schwierigen Situation stecken oder sich allgemein Weiterentwickeln und sich besser kennenlernen möchten." />
+	content="Ich biete Einzelberatung an für Menschen, die immer bei ähnlichen Problemen anstehen, in einer schwierigen Situation stecken oder sich besser kennenlernen und weiterentwickeln möchten." />
 </svelte:head>
 
 <BackgroundBeams></BackgroundBeams>
@@ -17,26 +17,28 @@
   
     <h2 class="w-full text-start text-xl sm:text-2xl font-semibold mt-4">Allgemein</h2>
     <p class="w-full text-start ml-10">
-      Psychologische Beratung ist keine Therapie<br>
-      Ich als Beraterin gebe Ihnen keine vorgefertigten Lösungen, sondern unterstütze Sie im herausfinden der eigenen Lösungen als Gesprächs- und Interaktionspartnerin.<br>
+      Psychologische Beratung ist keine Therapie.<br>
+      Als Beraterin gebe ich Ihnen keine vorgefertigten Lösungen, sondern unterstütze Sie dabei eigene Lösungen zu entwickeln und Ihren Themen
+      auf den Grund zu gehen.<br>
       Die Grundvoraussetzung hierfür ist, dass die Fähigkeit zu Selbststeuerung nicht eingeschränkt ist.
     </p>
 
     <h2 class="w-full text-start text-xl sm:text-2xl font-semibold mt-4">Vorgehen</h2>
     <p class="w-full text-start ml-10">
-      Ein  Ziel wird formuliert über einen bestimmten Zeitraum, in dem die notwendigen Entwicklungsschritte erarbeitet werden sollen und können.<br>
+      Gemeinsam formulieren wir Ihre Entwicklungsziele und legen die notwendigen Schritte fest über einen bestimmten Zeitraum.<br>
       <br>
-      Bestehende Verhaltensmuster werden bewusst gemacht und reflektiert.
-      Dabei bin ich an Ihrer Seite um Ihren individuellen Entwicklungsprozess zu fördern, Sie Ihre Stärken und Ressourcen bewusst werden zu lassen.
+      Wir eruieren gemeinsam bestehende Verhaltensmuster, machen diese bewusst und reflektieren sie.
+  
+      Ich unterstütze Sie in Ihrem individuellen Entwicklungsprozess und beim Erkennen Ihrer Stärken und Ressourcen.
       Ebenso assistiere ich Ihnen beim Entwickeln neuer Wahrnehmungsweisen und Handlungsmöglichkeiten.<br>
       <br>
-      Ist der geplante Zeitpunkt erreicht, überprüfen wir gemeinsam, ob Ihr Ziel erreicht ist oder ggf. weitere Maßnahmen notwendig sind.<br>
+      Wenn der geplante Zeitpunkt da ist, überprüfen wir gemeinsam, ob Ihr Ziel erreicht ist oder ob weitere Schritte notwendig sind.<br>
     </p>
 
     <TextGenerateEffect 
       segments={[
         { 
-          text: "«Die reinste Form des Wahnsinns ist es, alles beim Alten zu belassen und zu hoffen, dass sich etwas ändert»",
+          text: "«Die reinste Form des Wahnsinns ist es, alles beim Alten zu belassen und zu hoffen, dass sich etwas ändert.»",
           className: "text-xl sm:text-3xl font-medium"
         }, {
           text: "", className: "", newLine: true,
